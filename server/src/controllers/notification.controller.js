16bc1bca35a3d69c29e1311f3efd98a27a1fef7c
AhMeD <ahmedamine.benaifa@gmail.com>
2026-07-25 11:03:21 +0100

    fix another notif bug

--- server/src/controllers/notification.controller.js
+++ server/src/controllers/notification.controller.js
@@ -2,6 +2,7 @@ import * as notificationService from '../services/notification.service.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 
 export const getMyNotifications = asyncHandler(async (req, res) => {
+  res.set('Cache-Control', 'no-store');
   const notifications = await notificationService.getNotificationsForUser(req.user.id);
   res.status(200).json({ status: 'success', data: notifications });
 });
